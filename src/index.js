7f1ac16d8ab144b1eba72be2cb8665be01b234d6
unknown <nik.152002@mail.ru>
2020-10-25 21:26:49 +0300

    Implemented basic functions of the game

--- src/index.js
+++ src/index.js
@@ -3,31 +3,61 @@ import ReactDOM, { render } from 'react-dom';
 import './index.css';
 
 
-class Square extends React.Component {
-	render() {
-	  return (
-		 <button
+function calculateWinner(squares)
+{
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		
+	 ];
+	 for(let i = 0; i < lines.length;i++)
+	 {
+		 const [a,b,c] = lines[i];
+		 if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+			 return squares[a]
+		 }
+	 }
+	 return null;
+}
+
+
+function Square(props)
+{
+	return(
+		<button
 			className="square"
-			onClick={() => this.props.onClick()}
-		 >
-			{this.props.value}
-		 </button>
-	  );
-	}
- }
+			onClick={props.onClick}
+			>
+			{props.value}
+		</button>
+	)
+}
+
  
  class Board extends React.Component {
 	constructor(props) {
 	  super(props);
 	  this.state = {
 		 squares: Array(9).fill(null),
+		 xIsNext: true
 	  };
 	}
  
 	handleClick(i) {
 	  const squares = this.state.squares.slice(); // иммутабельность превышего всего
-	  squares[i] = 'X';
-	  this.setState({squares: squares}); // меняю стейт
+	  if(calculateWinner(squares) || typeof squares[i] === "string")
+	  		return // если победитель есть или клеточка не пустая, клик не засчитывается
+	  squares[i] = this.state.xIsNext ? "X" : " O";
+	  this.setState(
+		  {
+			  squares: squares,
+			  xIsNext: !this.state.xIsNext
+			}
+		  ); // меняю стейт
 	}
  
 	renderSquare(i) {
@@ -40,7 +70,10 @@ class Square extends React.Component {
 	}
  
 	render() {
-	  const status = 'Next player: X';
+	  const winner = calculateWinner(this.state.squares);
+	  let status;
+	  if(winner) status = "Winner : " + winner;
+	  else status = 'Next player: ' + (this.state.xIsNext ? "X" : "O"); 
 	  return (
 		 <div>
 			<div className="status">{status}</div>
@@ -80,14 +113,9 @@ class Square extends React.Component {
 	}
  }
  
- // ========================================
  
  ReactDOM.render(
 	<Game />,
 	document.getElementById('root')
  );
  
-
- 
-
-
