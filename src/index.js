67f3ba8c3ff3f1edd0ac557797c4d6aac93e528c
unknown <nik.152002@mail.ru>
2020-10-25 21:59:28 +0300

    Lifted state to the Game component

--- src/index.js
+++ src/index.js
@@ -5,27 +5,26 @@ import './index.css';
 
 function calculateWinner(squares)
 {
-	const lines = [
+	const lines = [ // перечисление всех возможных случаев победы
 		[0, 1, 2],
 		[3, 4, 5],
 		[6, 7, 8],
 		[0, 3, 6],
 		[1, 4, 7],
 		[2, 5, 8],
-		
 	 ];
 	 for(let i = 0; i < lines.length;i++)
 	 {
-		 const [a,b,c] = lines[i];
-		 if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+		 const [a,b,c] = lines[i]; // деструктуризация "победной линии" в переменные a,b,c - индексы предпологаемых победных клеток
+		 if(squares[a] !== null && squares[a] === squares[b] && squares[a] === squares[c])
 			 return squares[a]
-		 }
+		 
 	 }
-	 return null;
+	 return null; // дефолтный случай
 }
 
 
-function Square(props)
+function Square(props) // Square - зависимый компонент(controlled component), значит, он не имеет собственного state. Конструткор не нужен
 {
 	return(
 		<button
@@ -39,44 +38,19 @@ function Square(props)
 
  
  class Board extends React.Component {
-	constructor(props) {
-	  super(props);
-	  this.state = {
-		 squares: Array(9).fill(null),
-		 xIsNext: true
-	  };
-	}
- 
-	handleClick(i) {
-	  const squares = this.state.squares.slice(); // иммутабельность превышего всего
-	  if(calculateWinner(squares) || typeof squares[i] === "string")
-	  		return // если победитель есть или клеточка не пустая, клик не засчитывается
-	  squares[i] = this.state.xIsNext ? "X" : " O";
-	  this.setState(
-		  {
-			  squares: squares,
-			  xIsNext: !this.state.xIsNext
-			}
-		  ); // меняю стейт
-	}
  
 	renderSquare(i) {
 	  return (
 		 <Square
-			value={this.state.squares[i]}
-			onClick={() => this.handleClick(i)}
+			value={this.props.squares[i]}
+			onClick={() => this.props.onClick(i)} // лесенка пропсов
 		 />
 	  );
 	}
  
-	render() {
-	  const winner = calculateWinner(this.state.squares);
-	  let status;
-	  if(winner) status = "Winner : " + winner;
-	  else status = 'Next player: ' + (this.state.xIsNext ? "X" : "O"); 
+	render() { // отрисовка доски
 	  return (
 		 <div>
-			<div className="status">{status}</div>
 			<div className="board-row">
 			  {this.renderSquare(0)}
 			  {this.renderSquare(1)}
@@ -98,14 +72,50 @@ function Square(props)
  }
  
  class Game extends React.Component {
+	 constructor(props)
+	 {
+		 super(props);
+		 this.state = {
+			 history: [{ // история ходов
+					 squares : Array(9).fill(null)
+			}],
+			xIsNext: true // ходил ли "X"
+		 }
+	 }
+	 handleClick(i) {
+		const history = this.state.history; // создаю копию истории
+		const current = history[history.length - 1]; // беру текущее поле
+		const squares = current.squares; // беру текущее расположение крестиков и ноликов на поле
+		if(calculateWinner(squares) || typeof squares[i] === "string") 
+				return // если победитель есть или клеточка не пустая, клик не засчитывается
+		squares[i] = this.state.xIsNext ? "X" : "O";
+		this.setState(
+			{
+			history: [ // обновление истории ходов
+				...history,
+				{squares: squares}
+			],
+			xIsNext: !this.state.xIsNext,	
+			}
+			); // меняю стейт
+	 }
 	render() {
+		const history = this.state.history;
+		const current = history[history.length - 1];
+		const winner = calculateWinner(current.squares)
+		let status;
+		if(winner) status = "Winner : " + winner;
+		else status = "Next player : " + (this.state.xIsNext ? "X" : "O");
 	  return (
 		 <div className="game">
 			<div className="game-board">
-			  <Board />
+			  <Board 
+			  squares={current.squares}
+			  onClick={(i) => this.handleClick(i)}
+			  />
 			</div>
 			<div className="game-info">
-			  <div>{/* status */}</div>
+			  <div>{status}</div>
 			  <ol>{/* TODO */}</ol>
 			</div>
 		 </div>
@@ -114,7 +124,7 @@ function Square(props)
  }
  
  
- ReactDOM.render(
+ ReactDOM.render( // отрисовка приложения в элемент с id="root"
 	<Game />,
 	document.getElementById('root')
  );
